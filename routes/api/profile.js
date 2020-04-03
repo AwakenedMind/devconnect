0121d79cd20e227d2a7cfc16df3a667c4fa436c4
Robert A <daspanisharmada@gmail.com>
2020-04-03 04:26:29 -0700

    get all profiles & profile by userid

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -108,4 +108,43 @@ router.post(
   }
 );
 
+//@route GET api/profile
+//@desc  GET all profiles
+//@access Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route GET api/profile/user/:user_id
+//@desc  GET profile by User ID
+//@access Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profiles = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profiles);
+  } catch (err) {
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
