c3627e405d1f948ba6adc11fc8d255a8dabcce54
Robert A <daspanisharmada@gmail.com>
2020-04-04 10:59:59 -0700

    added frontend client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>Hi</h1>
+    </Fragment>
+  );
+};
+
+export default App;
