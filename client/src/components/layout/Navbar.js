aea1569b5e42cd666783dc63c4515ce6e8c69e04
Robert A <daspanisharmada@gmail.com>
2020-04-06 08:37:46 -0700

    navbar auth support

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
-    
+    <ul>
+      <li>
+        <a onClick={logout} href='#!'>
+          <i className='fas fa-sign-out-alt'></i>
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
   );
 
   const guestLinks = (
     <ul>
       <li>
-        <Link to='!#'>Developers</Link>
+        <Link to='#!'>Developers</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
@@ -27,6 +34,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
       <h1>
         <Link to='/'>DevConnector</Link>
       </h1>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
